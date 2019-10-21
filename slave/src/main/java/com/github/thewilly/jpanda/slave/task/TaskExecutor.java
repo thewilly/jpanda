58eb89de6ee8580449b0f8034da62d4ea6ea994a
Willy <colunga91@gmail.com>
2019-10-21 18:53:41 +0200

    [master] defines the task executor interface

--- slave/src/main/java/com/github/thewilly/jpanda/slave/task/TaskExecutor.java
+++ slave/src/main/java/com/github/thewilly/jpanda/slave/task/TaskExecutor.java
@@ -3,8 +3,16 @@ package com.github.thewilly.jpanda.slave.task;
 import com.github.thewilly.jpanda.core.task.Task;
 import com.github.thewilly.jpanda.core.task.TaskResult;
 
+/**
+ * The interface Task executor.
+ */
 public interface TaskExecutor {
 
+    /**
+     * Execute task result.
+     *
+     * @return the task result
+     */
     TaskResult execute();
 
 }
